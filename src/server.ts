a00676f1feec4d6792023fc36fbbd509cc63468a
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-04-03 16:01:11 -0300

    Rota para criar um evento e validação de erro

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import fastify from "fastify";
+import { createEvent } from "./routes/create-event";
 
 const app = fastify();
 
-app.get("/", async () => {
-  return "Hello World!!";
-});
+app.register(createEvent);
 
 app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running on port: http://localhost:3333");
